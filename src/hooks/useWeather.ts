307025c4a3a50fb97b60e3df804b4e852138e603
mariano martin velasco <mariano18martin@gmail.com>
2026-06-14 19:45:25 +0200

    Proyecto de clima de tiempo actualizacion de url de http a https

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -71,7 +71,7 @@ export default function useWeather(){
 
             const appId = import.meta.env.VITE_API_KEY
 
-            const geoUrl =`http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+            const geoUrl =`https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
 
             const {data} = await axios(geoUrl)
 
